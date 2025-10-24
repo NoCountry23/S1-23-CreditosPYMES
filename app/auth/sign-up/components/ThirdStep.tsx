597ebbaebfed61386e1a686b5a3133e69e305dc5
LautaroLD <duranlautarogabriel@gmail.com>
2025-10-24 18:16:29 -0300

    feat: Refactor SignUpForm and ThirdStep components for improved functionality and validation
    - Updated SignUpForm to include default values for user and company data.
    - Enhanced file upload handling in SignUpForm.
    - Created separate functions for user and company creation, and document upload.
    - Integrated toast notifications for error handling in SignUpForm.
    - Refactored ThirdStep to include additional validation and improved layout.
    - Changed select inputs to text inputs where appropriate for better user experience.
    - Updated global CSS for consistent styling across components.
    - Modified ESLint configuration to enforce semicolons and double quotes.

--- app/auth/sign-up/components/ThirdStep.tsx
+++ app/auth/sign-up/components/ThirdStep.tsx
@@ -1,100 +1,110 @@
-import React from "react";
 import {
+  Controller,
   FieldError,
   FieldErrorsImpl,
   Merge,
   useFormContext,
 } from "react-hook-form";
 import { FormDataSignUp } from "./SignUpForm";
+import { ArrowLeft, ArrowRight } from "lucide-react";
+import Link from "next/link";
 
-export default function ThirdStep() {
+export default function ThirdStep({
+  handleNext,
+  handlePrevious,
+}: {
+  handleNext: () => void
+  handlePrevious: () => void
+}) {
   const {
     register,
+    control,
     formState: { errors },
   } = useFormContext();
   const legalErrors = errors?.legalInfo as Merge<
     FieldError,
     FieldErrorsImpl<FormDataSignUp["legalInfo"]>
   >;
+
   return (
-    <div className="space-y-5">
-      <h2 className="text-2xl font-bold  mb-6">
+    <div className='space-y-5'>
+      <h2 className='text-2xl font-bold  mb-6'>
         Información Legal y Financiera
       </h2>
 
       <div>
-        <label className="block text-sm font-medium  mb-2">
+        <label className='block text-sm font-medium  mb-2'>
           Dirección Fiscal *
         </label>
         <input
-          type="text"
+          type='text'
           {...register("legalInfo.address", {
             required: "La dirección es requerida",
             minLength: { value: 5, message: "Dirección muy corta" },
           })}
-          className={`w-full px-4 py-3 border rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent ${
+          className={`w-full px-4 py-3 outline-none border border-gray-500/50 bg-input rounded-lg focus:ring-2 focus:ring-blue-500 ${
             legalErrors?.address ? "border-red-500" : "border-gray-500"
           }`}
-          placeholder="Av. Corrientes 1234, Piso 5, Oficina B"
+          placeholder='Av. Corrientes 1234, Piso 5, Oficina B'
         />
         {legalErrors?.address && (
-          <p className="mt-1 text-sm text-red-600">
+          <p className='mt-1 text-sm text-red-600'>
             {legalErrors?.address.message}
           </p>
         )}
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
+      <div className='grid grid-cols-1 md:grid-cols-3 gap-5'>
         <div>
-          <label className="block text-sm font-medium  mb-2">Ciudad *</label>
+          <label className='block text-sm font-medium  mb-2'>Ciudad *</label>
           <input
-            type="text"
+            type='text'
             {...register("legalInfo.city", {
               required: "La ciudad es requerida",
             })}
-            className={`w-full px-4 py-3 border rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent ${
+            className={`w-full px-4 py-3 outline-none border border-gray-500/50 bg-input rounded-lg focus:ring-2 focus:ring-blue-500 ${
               legalErrors?.city ? "border-red-500" : "border-gray-500"
             }`}
-            placeholder="Buenos Aires"
+            placeholder='Buenos Aires'
           />
           {legalErrors?.city && (
-            <p className="mt-1 text-sm text-red-600">
+            <p className='mt-1 text-sm text-red-600'>
               {legalErrors?.city.message}
             </p>
           )}
         </div>
 
         <div>
-          <label className="block text-sm font-medium  mb-2">Provincia *</label>
+          <label className='block text-sm font-medium  mb-2'>Provincia *</label>
           <select
             {...register("legalInfo.province", {
               required: "Selecciona una provincia",
             })}
-            className={`w-full px-4 py-3 border rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent ${
+            className={`w-full px-4 py-3 outline-none border border-gray-500/50 bg-input rounded-lg focus:ring-2 focus:ring-blue-500 ${
               legalErrors?.province ? "border-red-500" : "border-gray-500"
             }`}
           >
-            <option value="">Seleccionar...</option>
-            <option value="CABA">CABA</option>
-            <option value="Buenos Aires">Buenos Aires</option>
-            <option value="Córdoba">Córdoba</option>
-            <option value="Santa Fe">Santa Fe</option>
-            <option value="Mendoza">Mendoza</option>
-            <option value="Tucumán">Tucumán</option>
+            <option value=''>Seleccionar...</option>
+            <option value='CABA'>CABA</option>
+            <option value='Buenos Aires'>Buenos Aires</option>
+            <option value='Córdoba'>Córdoba</option>
+            <option value='Santa Fe'>Santa Fe</option>
+            <option value='Mendoza'>Mendoza</option>
+            <option value='Tucumán'>Tucumán</option>
           </select>
           {legalErrors?.province && (
-            <p className="mt-1 text-sm text-red-600">
+            <p className='mt-1 text-sm text-red-600'>
               {legalErrors?.province.message}
             </p>
           )}
         </div>
 
         <div>
-          <label className="block text-sm font-medium  mb-2">
+          <label className='block text-sm font-medium  mb-2'>
             Código Postal *
           </label>
           <input
-            type="text"
+            type='text'
             {...register("legalInfo.zipCode", {
               required: "El código postal es requerido",
               pattern: {
@@ -102,70 +112,75 @@ export default function ThirdStep() {
                 message: "Formato: 4 dígitos",
               },
             })}
-            className={`w-full px-4 py-3 border rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent ${
+            className={`w-full px-4 py-3 outline-none border border-gray-500/50 bg-input rounded-lg focus:ring-2 focus:ring-blue-500 ${
               legalErrors?.zipCode ? "border-red-500" : "border-gray-500"
             }`}
-            placeholder="1234"
+            placeholder='1234'
           />
           {legalErrors?.zipCode && (
-            <p className="mt-1 text-sm text-red-600">
+            <p className='mt-1 text-sm text-red-600'>
               {legalErrors?.zipCode.message}
             </p>
           )}
         </div>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
         <div>
-          <label className="block text-sm font-medium  mb-2">
+          <label className='block text-sm font-medium  mb-2'>
             Cantidad de Empleados *
           </label>
-          <select
+          <input
+            type='text'
             {...register("legalInfo.employees", {
-              required: "Selecciona una opción",
+              required: "La cantidad de empleados es requerida",
             })}
-            className={`w-full px-4 py-3 border rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent ${
+            className={`w-full px-4 py-3 outline-none border border-gray-500/50 bg-input rounded-lg focus:ring-2 focus:ring-blue-500 ${
               legalErrors?.employees ? "border-red-500" : "border-gray-500"
             }`}
-          >
-            <option value="">Seleccionar...</option>
-            <option value="1-5">1-5 empleados</option>
-            <option value="6-10">6-10 empleados</option>
-            <option value="11-25">11-25 empleados</option>
-            <option value="26-50">26-50 empleados</option>
-            <option value="51-100">51-100 empleados</option>
-            <option value="100+">Más de 100</option>
-          </select>
+            placeholder='10'
+          />
           {legalErrors?.employees && (
-            <p className="mt-1 text-sm text-red-600">
+            <p className='mt-1 text-sm text-red-600'>
               {legalErrors?.employees.message}
             </p>
           )}
         </div>
 
         <div>
-          <label className="block text-sm font-medium  mb-2">
+          <label className='block text-sm font-medium  mb-2'>
             Facturación Anual Estimada *
           </label>
-          <select
-            {...register("legalInfo.estimatedAnnualBilling", {
-              required: "Selecciona una opción",
-            })}
-            className={`w-full px-4 py-3 border rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent ${
-              legalErrors?.estimatedAnnualBilling
-                ? "border-red-500"
-                : "border-gray-500"
-            }`}
-          >
-            <option value="">Seleccionar...</option>
-            <option value="0-500k">Hasta $500.000</option>
-            <option value="500k-1M">$500.000 - $1.000.000</option>
-            <option value="1M-5M">$1.000.000 - $5.000.000</option>
-            <option value="5M-10M">$5.000.000 - $10.000.000</option>
-            <option value="10M+">Más de $10.000.000</option>
-          </select>
+          <Controller
+            name='legalInfo.estimatedAnnualBilling'
+            control={control}
+            rules={{
+              required: "La facturación anual estimada es obligatoria",
+            }}
+            render={({ field }) => (
+              <input
+                {...field}
+                className={`w-full px-4 py-3 outline-none border border-gray-500/50 bg-input rounded-lg focus:ring-2 focus:ring-blue-500 ${
+                  legalErrors?.estimatedAnnualBilling
+                    ? "border-red-500"
+                    : "border-gray-500"
+                }`}
+                value={Intl.NumberFormat("en-US", {
+                  style: "currency",
+                  currency: "USD",
+
+                  minimumFractionDigits: 0,
+                }).format(Number(field.value || 0))}
+                onChange={(e) => {
+                  const raw = e.target.value.replace(/[^0-9]/g, "");
+                  field.onChange(raw);
+                }}
+              />
+            )}
+          />
+
           {legalErrors?.estimatedAnnualBilling && (
-            <p className="mt-1 text-sm text-red-600">
+            <p className='mt-1 text-sm text-red-600'>
               {legalErrors?.estimatedAnnualBilling.message}
             </p>
           )}
@@ -173,15 +188,51 @@ export default function ThirdStep() {
       </div>
 
       <div>
-        <label className="block text-sm font-medium  mb-2">
-          Descripción de la Actividad
+        <label className='block text-sm font-medium  mb-2'>
+          Descripción de la Actividad *
         </label>
         <textarea
-          {...register("legalInfo.activityDescription")}
+          {...register("legalInfo.activityDescription", {
+            required: "La descripción de la actividad es requerida",
+          })}
           rows={4}
-          className="w-full px-4 py-3 border border-gray-500/50 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-          placeholder="Describe brevemente la actividad principal de tu empresa..."
+          className='w-full px-4 py-3 outline-none border border-gray-500/50 bg-input rounded-lg focus:ring-2 focus:ring-blue-500'
+          placeholder='Describe brevemente la actividad principal de tu empresa...'
         ></textarea>
+
+        {legalErrors?.activityDescription && (
+          <p className='mt-1 text-sm text-red-600'>
+            {legalErrors?.activityDescription.message}
+          </p>
+        )}
+      </div>
+      <div className='flex justify-between flex-col lg:flex-row '>
+        <button
+          type='button'
+          onClick={handlePrevious}
+          className='flex items-center border border-slate-500/50 justify-center gap-2 px-6 py-3  text-gray rounded-lg hover:text-gray-500 transition-all font-medium'
+        >
+          <ArrowLeft className='w-4 h-4' />
+          Anterior
+        </button>
+
+        <div className='flex items-center justify-center gap-3'>
+          <Link
+            href='/auth/login'
+            className='px-6 py-3 text-gray-600 hover:text-gray-800 transition-all'
+          >
+            ¿Ya tienes cuenta? Inicia sesión
+          </Link>
+        </div>
+
+        <button
+          type='button'
+          onClick={handleNext}
+          className='flex items-center justify-center gap-2 px-6 py-3 bg-gradient-to-r from-blue-600 to-blue-700  rounded-lg hover:from-blue-700 hover:to-blue-800 transition-all font-medium shadow-lg hover:shadow-xl'
+        >
+          Siguiente
+          <ArrowRight className='w-4 h-4' />
+        </button>
       </div>
     </div>
   );
